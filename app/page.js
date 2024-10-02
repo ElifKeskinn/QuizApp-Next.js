5aeeb06a0f72ac91fc65e984d6f0e2f4a3738bcc
Elif <elif.keskin233@gmail.com>
2024-10-02 20:09:28 +0300

    stling for homePage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import './styles/quizPage.css';
@@ -14,6 +14,7 @@ export default function HomePage() {
     setSelectedTopic(topic);
     router.push(`/quiz/${topic}`);
   };
+
 
   return (
     <div className="StartPage">
