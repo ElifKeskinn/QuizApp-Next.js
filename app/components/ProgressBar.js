f9c2cbc06963ff31c6269111ee9d6f2651fed7ac
Elif <elif.keskin233@gmail.com>
2024-10-02 20:33:05 +0300

    made components client side

--- app/components/ProgressBar.js
+++ app/components/ProgressBar.js
@@ -1,4 +1,4 @@
- 'use server'
+ 'use client'
  import '../styles/ProgressBar.css';
 
 const ProgressBar = ({ progress }) => (
--- app/components/Question.js
+++ app/components/Question.js
@@ -1,4 +1,4 @@
-'use server'
+'use client'
 const Question = ({ question, onAnswerSelect, selectedAnswer, onSubmitAnswer, isSubmitted }) => (
   <div className="question">
     <h2>{question.question}</h2>
