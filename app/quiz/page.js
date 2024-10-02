7ef72482970cbbb144d678246b03ef77dc42e75c
Elif <elif.keskin233@gmail.com>
2024-10-02 20:44:42 +0300

    writed a basic code for deployment

---
+++ app/quiz/page.js
@@ -0,0 +1,10 @@
+const QuizPage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Quiz App</h1>
+            <p>Get ready to test your knowledge!</p>
+        </div>
+    );
+};
+
+export default QuizPage;
